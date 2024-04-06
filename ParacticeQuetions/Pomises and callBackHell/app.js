430f5dbfb2772922740198cdc7e459ffd239c23c
shivrajkaudare <shivrajkaudare@gmail.com>
2024-04-06 11:55:52 +0530

    Async-Await in jS

--- ParacticeQuetions/Pomises and callBackHell/app.js
+++ ParacticeQuetions/Pomises and callBackHell/app.js
@@ -1,63 +1,63 @@
 
-// function savetoDb(data, sucess, failure){
-//     let internateSpeed = Math.floor(Math.random()* 10)+1;
-//     if(internateSpeed > 4){
-//         sucess();
-//     }else{
-//         failure();
-//     }
+// // function savetoDb(data, sucess, failure){
+// //     let internateSpeed = Math.floor(Math.random()* 10)+1;
+// //     if(internateSpeed > 4){
+// //         sucess();
+// //     }else{
+// //         failure();
+// //     }
+// // }
+
+// // // call-back Hell
+// // savetoDb(
+// //     "hello", 
+// //     () => {
+// //     console.log("sucess: your data was saved");
+// //     savetoDb("hello word",()=>{
+// //         console.log("success2: data2 was saved");
+// //     }, () =>{
+// //         console.log("failure2: weak connection");
+// //     })
+// // },
+// // () => {
+// //     console.log("Failure: your data was not saved");
+// // }
+// // );
+
+
+// ///---- Promises ----
+// // 1)- resolve & reject
+
+// function savetoDb(data){
+//     return new Promise((resolve, reject) => {
+//        let internateSpeed = Math.floor(Math.random()* 10) + 1;
+//        if(internateSpeed > 4){
+//         resolve("success : data was saved");
+//        }else{
+//         reject("failure : weak connection");
+//        }
+
+//     });
 // }
 
-// // call-back Hell
-// savetoDb(
-//     "hello", 
-//     () => {
-//     console.log("sucess: your data was saved");
-//     savetoDb("hello word",()=>{
-//         console.log("success2: data2 was saved");
-//     }, () =>{
-//         console.log("failure2: weak connection");
-//     })
-// },
-// () => {
-//     console.log("Failure: your data was not saved");
-// }
-// );
+// savetoDb("Shivraj Kaudare");
 
 
-///---- Promises ----
-// 1)- resolve & reject
+// // 2) then() & catch()
 
-function savetoDb(data){
-    return new Promise((resolve, reject) => {
-       let internateSpeed = Math.floor(Math.random()* 10) + 1;
-       if(internateSpeed > 4){
-        resolve("success : data was saved");
-       }else{
-        reject("failure : weak connection");
-       }
+// // let request = savedatatoDb("Hii there iam shivraj");
 
-    });
-}
-
-savetoDb("Shivraj Kaudare");
-
-
-// 2) then() & catch()
-
-// let request = savedatatoDb("Hii there iam shivraj");
-
-//  request.then(() => {
-//     console.log("promise resolved");
-//  })
-//  .catch(() => {
-//     console.log("Promise Rejected");
-//  });
- // OR we can write it in another way
-savetoDb("Hii there iam shivraj kaudare")
-.then(() => {
-    console.log("promise Was Resolved");
-})
-.catch(() => {
-    console.log("promise was rejected")
-});
+// //  request.then(() => {
+// //     console.log("promise resolved");
+// //  })
+// //  .catch(() => {
+// //     console.log("Promise Rejected");
+// //  });
+//  // OR we can write it in another way
+// savetoDb("Hii there iam shivraj kaudare")
+// .then(() => {
+//     console.log("promise Was Resolved");
+// })
+// .catch(() => {
+//     console.log("promise was rejected")
+// });
---
+++ ParacticeQuetions/Pomises and callBackHell/async-await.js
@@ -0,0 +1,27 @@
+  
+  // async- await 
+
+  let h1 = document.querySelector('h1');
+
+ function colorChange(color, delay){
+
+    return new Promise((resolve, reject) =>{
+
+        setTimeout(() =>{
+            h1.style.color = color;
+            console.log(`color changed to ${color}`);
+            resolve("color is changed");
+        }, delay)
+        
+     })
+  }
+
+  async function demo(){
+    colorChange("green",1000);
+    await colorChange("Blue", 1000);
+    await colorChange("yellow", 1000);
+    await colorChange("orange", 1000);
+  };
+
+
+  demo();
