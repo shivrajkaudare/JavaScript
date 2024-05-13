bd60034c7333e1dfabee10bf8d200fca3d444b52
Shivraj Kaudare <shivrajkaudare@gmail.com>
2024-05-13 14:24:04 +0530

    Update RandomNumber.js

--- Guessing-Game/RandomNumber.js
+++ Guessing-Game/RandomNumber.js
@@ -1,1 +1,23 @@
 
+const max = prompt("enter the max number :");
+
+const random = Math.floor(Math.random()*max) + 1;
+
+let guess = prompt("guess the number");
+
+while(true){
+    if( guess == "quit"){
+        console.log("user quit");
+        break;
+    }
+    if(guess == random){
+        console.log("You are right😊 ! congrats !! random Number was :",random);
+        break;
+    }
+    else if(guess < random){
+       guess= prompt(" hint : Your guess was too small. please try again 😒");
+    }
+    else{
+        guess = prompt("hint : your guess was too large. please guess again");
+    }
+}
