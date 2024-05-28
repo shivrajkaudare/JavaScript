530f5c10838a30c37e4550d3392b063c89b26121
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-05-28 11:57:57 +0530

    Update asyncFunctions.js

---
+++ Promises/asyncFunctions.js
@@ -0,0 +1,21 @@
+// async functions always return a Promise.
+// await- used to wait for promise and gets its fullfilled value.
+
+// ex.- a function which generates a three random number each number is generated after 1 sec (use async and await keywords).
+
+function getNum() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let randomNum = Math.floor(Math.random * 10) + 1;
+      console.log(randomNum);
+      resolve("number is genereted");
+    });
+  });
+}
+
+// async finction and await keyword.
+async function demo() {
+  await getNum();
+  await getNum();
+  getNum();
+}
