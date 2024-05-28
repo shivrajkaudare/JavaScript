bae2441cfe11f71fdfb11d201271ccfce9a2d718
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-05-28 09:46:14 +0530

    Update app.js

--- Promises/app.js
+++ Promises/app.js
@@ -1,1 +1,20 @@
+/*
+ resolve and reject keywords-
+  resolve : promise fullfilled / success.
+  reject : failure 
+*/
+
+function saveData(data) {
+  return new Promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 4) {
+      resolve("Data Saved Successfully");
+    } else {
+      reject("Slow network..Try again later");
+    }
+  });
+}
+
+saveData("Hello my name is Shiv");
+
 // when JS API's request to the server promises promise that no matter request failure or successful it always returns a response.
