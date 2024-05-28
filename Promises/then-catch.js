053765c8962855b66c1a38befc2afa77bfdf1015
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-05-28 10:20:43 +0530

    Create then-catch.js

---
+++ Promises/then-catch.js
@@ -0,0 +1,27 @@
+/*
+ then() and catch() methods -
+
+ then() : if we want to do some works or print something if promise is fullfilled.
+ catch() : catch() catch the errors if promise is rejected.
+*/
+
+function savetoDB(data) {
+  return new Promise((resolve, reject) => {
+    let internetSpeed = Math.floor(Math.random() * 10) + 1;
+    if (internetSpeed > 4) {
+      resolve("Data is saved to databased Successfully");
+    } else {
+      reject("Slow Newwork..Please Try again Later...");
+    }
+  });
+}
+
+let request = savetoDB("Hello Dear JS lovers..");
+
+request
+  .then(() => {
+    console.log("Promise is resolved");
+  })
+  .catch(() => {
+    console.log("Promise is rejected");
+  });
