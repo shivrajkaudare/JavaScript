e5f387db97351a5308f4b121c1f07f4e6d1f4222
Shivraj Kaudare <131277824+shivrajkaudare@users.noreply.github.com>
2024-05-29 10:49:18 +0530

    Update FeachAPI-Request.js

--- Promises/FeachAPI-Request.js
+++ Promises/FeachAPI-Request.js
@@ -1,2 +1,16 @@
 // Fetch is used to make API's Request in JavaScript (HTTP Request).
 // Fetch is used to fetch Resources from server.
+
+let url = "https://catfact.ninja/facts";
+
+fetch(url)
+  .then((res) => {
+    console.log(res);
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
